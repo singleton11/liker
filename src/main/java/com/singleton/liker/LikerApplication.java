f9555e65e8b85f75050363c14ae63ee8835c5fe9
Anton Prokhorov <betrayer11@gmail.com>
2018-08-12 12:44:01 +0300

    Initial commit

---
+++ src/main/java/com/singleton/liker/LikerApplication.java
@@ -0,0 +1,12 @@
+package com.singleton.liker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikerApplication.class, args);
+    }
+}
